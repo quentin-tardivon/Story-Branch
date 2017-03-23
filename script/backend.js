08513fdffc7a1343b456dbb73ea902604c310b5c
Quentin TARDIVON <quentin.tardivon@gmail.com>
2017-03-23 09:28:20 +0100

    Death animation and page, step1,2,3,4 content

--- script/backend.js
+++ script/backend.js
@@ -8,6 +8,21 @@ function decreaseScore() {
   document.getElementById("score").innerHTML = "Your Score: " + remote.getGlobal('sharedObj').score;
 }
 
-function printResponse(response) {
+function printResponse(response, link) {
   document.getElementById("response").innerHTML = response;
+  document.getElementById("response").className = "mdc-ripple-surface demo-surface mdc-elevation--z2";
+
+  if (document.getElementById("response1") != null) {
+      document.getElementById("response1").onclick = "";
+  }
+
+  if (document.getElementById("response2") != null) {
+      document.getElementById("response2").onclick = "";
+  }
+
+  if (document.getElementById("response3") != null) {
+      document.getElementById("response3").onclick = "";
+  }
+
+
 }
